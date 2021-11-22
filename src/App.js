0b1fcbaf64849080a008762bd76036de75ff5775
Taufiq Wahid <taufiq.wahid58@gmail.com>
2021-11-23 05:31:53 +0800

    atomic design

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {SafeAreaView, StatusBar, StyleSheet, Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <StatusBar />
+      <Text>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo,
+        repellendus. Sit, sed ratione. Saepe, quibusdam, deserunt quos provident
+        harum dolores quasi illo quis reiciendis officiis optio consequatur
+        ratione facere omnis non iusto ullam. Animi expedita, accusamus natus
+        repellat laudantium modi sint in quasi, neque minima aperiam dolores
+        optio quidem iste debitis. Accusamus natus perspiciatis, distinctio
+        tenetur deserunt ab necessitatibus inventore, odio iusto aspernatur
+        dolor. Sunt saepe doloremque iste tenetur vel sapiente, culpa ad ab
+        minus quod recusandae eligendi molestias iure, tempore harum a odit
+        dolor consequuntur sequi. Ipsum quas ipsam, aspernatur neque ratione
+        nobis sit rerum commodi amet asperiores blanditiis.
+      </Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
