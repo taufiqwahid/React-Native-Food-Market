0bc0d80b3221780b381cb407bcecc894f92ed12a
Taufiq Wahid <taufiq.wahid58@gmail.com>
2021-12-05 16:28:39 +0800

    show message

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
 import {StyleSheet} from 'react-native';
+import FlashMessage from 'react-native-flash-message';
 import {Provider} from 'react-redux';
 import store from './redux/store';
 import Router from './router';
@@ -10,6 +11,7 @@ const App = () => {
     <NavigationContainer>
       <Provider store={store}>
         <Router />
+        <FlashMessage position="top" />
       </Provider>
     </NavigationContainer>
   );
